419915b90ef19a6ba6d61f95fdcabf5c17c7b36d
francorabini <francorabini03@gmail.com>
2024-10-22 19:50:27 -0300

    Parte A en casa

--- ej1labfpga.vhd
+++ ej1labfpga.vhd
@@ -2,8 +2,16 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL 
 entity ej1labfpga is 
 port ( SW1 : in STD_LOGIC;
-SW2 : in STD_LOGIC;
-SW3 : in STD_lOGIC;
-LED : out STD_LOGIC);
+			SW2 : in STD_LOGIC;
+			SW3 : in STD_lOGIC;
+			LED : out STD_LOGIC);
 end ej1labfpga;
-
+architecture Behavioral of ej1labfpga is
+	signal SIG1: std_logic;
+				SIG2: std_logic;
+begin
+	SIG1 <= ( not (SW2) xnor SW1);
+		SIG2 <= (not (SW2) and SW3);
+	LED <= SIG1 or SIG2;
+end behavioral	
+	
---
+++ ej1labfpga.vhd.bak
@@ -0,0 +1,9 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL 
+entity ej1labfpga is 
+port ( SW1 : in STD_LOGIC;
+SW2 : in STD_LOGIC;
+SW3 : in STD_lOGIC;
+LED : out STD_LOGIC);
+end ej1labfpga;
+
