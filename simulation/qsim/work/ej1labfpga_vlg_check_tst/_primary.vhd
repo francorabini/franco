d6bfb974ea7fc2bad7916161bdcfc9e2117c679d
francorabini <francorabini03@gmail.com>
2024-11-06 19:04:04 -0300

    ajustes

--- simulation/qsim/work/ej1labfpga_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ej1labfpga_vlg_check_tst is
-    port(
-        LED             : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end ej1labfpga_vlg_check_tst;
--- simulation/qsim/work/ej1labfpga_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ej1labfpga_vlg_vec_tst is
-end ej1labfpga_vlg_vec_tst;
